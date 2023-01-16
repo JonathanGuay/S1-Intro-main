97f60a1726fcc7568f7a67aa1bd8db43b38c921f
JonathanGuay <1424439@cegepoutaouais.qc.ca>
2023-01-16 10:47:33 -0500

    commit changement js

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ function ajouter(){
     var item = document.getElementById("desc").value;
 
     let nouveauP = document.createElement("p");              // créer une balise p // mettre un let
-    nouveauP.innerText = item;                                   // Écrire dans le paragraphe
+    nouveauP.innerText = item;                                      // Écrire dans le paragraphe
     document.getElementById("maListe").append(nouveauP);  // Ajouter le paragraphe à la liste
 
 
 
     // alert("L'item est bien ajouté"); //pop-up
 }
-nouveauInput = document.createElement("input")
+
+
+nouveauInput = document.createElement("input");
+nouveauInput =
 
 
 
